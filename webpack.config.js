5c506f045aee23a0619fc468959e3ad28c9ea297
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-07-30 10:49:10 -0700

    improve webpack config;

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
 
-const NODE_DEV_SERVER_URL = 'http://localhost:3000';
+const NODE_DEV_SERVER_URL = `http://localhost:${process.env.PORT}`;
 
 module.exports = {
   entry: path.resolve(__dirname, 'client/index'),
@@ -27,11 +26,12 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin(),
-  // ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   devServer: {
     contentBase: `${__dirname}/client/dist`,
+    historyApiFallback: true,
     hot: true,
     proxy: {
       '/api': NODE_DEV_SERVER_URL,
