13bda6544eb376989d23f35a5d8c3c649f8b8a28
TarlanG <qtarlan@yahoo.com>
2019-07-30 12:21:33 -0700

    create user router and create endpoint

---
+++ server/routers/userRouter.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const userController = require('../controllers/userController');
+
+const userRouter = new Router();
+
+userRouter.post('/create', userController.createUser, (req, res) => {
+  res.status(200).json({ success: true, message: 'user successfully created' });
+});
+
+
+
+module.exports = userRouter;
