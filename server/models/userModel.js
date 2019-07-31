b881d00a055ba314de68ae00985d656907a5edf5
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-07-30 19:08:46 -0700

    add getAllUsers method to userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -11,9 +11,11 @@ const CREATE_TABLE = `CREATE TABLE IF NOT EXISTS "user"(
 );`;
 
 const INSERT_USER = 'INSERT INTO "user" (username, token, email, picture_url, about_me) VALUES ($1, $2, $3, $4, $5)';
+const GET_ALL = 'SELECT * FROM "user"';
 
-pool.query(CREATE_TABLE, (err, _result) => {
-  if (err) console.log('error creating user table:', err);
+
+pool.query(CREATE_TABLE, (error, _result) => {
+  if (error) console.log('error creating user table:', error);
   else console.log('user table created or already existed');
 });
 
@@ -25,11 +27,20 @@ const model = {
       pool.query(
         INSERT_USER,
         [username, token, email, pictureUrl, aboutMe],
-        (err, result) => {
-          if (err) return reject(err);
+        (error, result) => {
+          if (error) return reject(error);
           return resolve(result);
         },
       );
+    });
+  },
+
+  getAllUsers() {
+    return new Promise((resolve, reject) => {
+      pool.query(GET_ALL, (error, result) => {
+        if (error) return reject(error);
+        return resolve(result);
+      });
     });
   },
 };
