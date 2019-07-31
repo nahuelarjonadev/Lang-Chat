e8272889c775f3214c9234ae53fc3abcf9275d81
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-07-30 20:05:40 -0700

    add getAllUsers and deleteUser queries

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -10,8 +10,10 @@ const CREATE_TABLE = `CREATE TABLE IF NOT EXISTS "user"(
   registration_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );`;
 
-const INSERT_USER = 'INSERT INTO "user" (username, token, email, picture_url, about_me) VALUES ($1, $2, $3, $4, $5)';
-const GET_ALL = 'SELECT * FROM "user"';
+const INSERT_USER = 'INSERT INTO "user" (username, token, email, picture_url, about_me, is_deleted) VALUES ($1, $2, $3, $4, $5) RETURNING user_id, username, email, picture_url, about_me';
+const GET_ALL = 'SELECT * FROM "user" ORDER BY $1 LIMIT $2 OFFSET $3';
+const GET_BY = 'SELECT * FROM "user" WHERE ';
+const DELETE_USER = 'UPDATE "user" SET is_deleted=true WHERE user_id = $1 RETURNING user_id, username, email, picture_url, about_me';
 
 
 pool.query(CREATE_TABLE, (error, _result) => {
@@ -23,21 +25,32 @@ const model = {
   createUser({
     username, token, email, pictureUrl, aboutMe,
   }) {
+    const params = [username, token, email, pictureUrl, aboutMe];
+
     return new Promise((resolve, reject) => {
-      pool.query(
-        INSERT_USER,
-        [username, token, email, pictureUrl, aboutMe],
-        (error, result) => {
-          if (error) return reject(error);
-          return resolve(result);
-        },
-      );
+      pool.query(INSERT_USER, params, (error, result) => {
+        if (error) return reject(error);
+        return resolve(result);
+      });
     });
   },
 
-  getAllUsers() {
+  getAllUsers({ order, limit, offset }) {
+    const params = [order || 'username', limit || 'ALL', offset || 0];
+
     return new Promise((resolve, reject) => {
-      pool.query(GET_ALL, (error, result) => {
+      pool.query(GET_ALL, params, (error, result) => {
+        if (error) return reject(error);
+        return resolve(result);
+      });
+    });
+  },
+
+  deleteUser(userId) {
+    const params = [userId];
+
+    return new Promise((resolve, reject) => {
+      pool.query(DELETE_USER, params, (error, result) => {
         if (error) return reject(error);
         return resolve(result);
       });
