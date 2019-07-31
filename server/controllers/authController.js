f2d5f9a4157ce6228f6ccd0a5a715e1c0d2f9809
nahuelarjonadev <25909829+nahuelarjonadev@users.noreply.github.com>
2019-07-31 13:33:27 -0700

    complete signup and signin middleware

---
+++ server/controllers/authController.js
@@ -0,0 +1,47 @@
+const userModel = require('../models/userModel');
+
+const authController = {
+  async signup(req, res, next) {
+    const userParams = {
+      username: req.body.username,
+      email: req.body.email,
+      pictureUrl: req.body.pictureUrl,
+      aboutMe: req.body.aboutMe,
+    };
+
+    userParams.token = createToken(req.body.password);
+
+    try {
+      const newUser = await userModel.createUser(userParams);
+      res.locals.user = newUser;
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async signin(req, res, next) {
+    const { username, password } = req.body;
+
+    try {
+      const { user, token } = await userModel.getUserAndTokenByUsername(username);
+      const isValidLogin = validatePassword(password, token);
+      if (!isValidLogin) return next(new Error('invalid username and/or password'));
+      res.locals.user = user;
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  },
+};
+
+function createToken(password) {
+  return password;
+}
+
+function validatePassword(password, user) {
+  if (!user) return false;
+  return createToken(password) === user.token;
+}
+
+module.exports = authController;
