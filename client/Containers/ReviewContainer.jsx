4b45b9da25653ad29b4eff2eb7bb15495137f180
Angel Vega <angelvega85@gmail.com>
2019-07-30 22:00:59 -0700

    added profile and review components and containers. ReviewCard functional

---
+++ client/Containers/ReviewContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReviewCard from '../Components/ReviewCard.jsx'
+
+function ReviewContainer() {
+  const reviewsArray = [{
+    conversationId: 1,
+    userId: 'Angel',
+    rating: '4/5',
+    language: 'English',
+    date: '07/26/19',
+    level: 'Fluent',
+    review: 'Nice person to have a conversation with. Love the accent. He played a song in english, so he gets a higher rating for that. One point off because he needs a shave.'
+  }]
+  const reviews = reviewsArray.map((reviewObj) => {
+      return <ReviewCard key={reviewObj.conversationId} reviewInfo={reviewObj}/>
+  })
+    return (
+      <ReviewContainerStyled>
+          <div>
+            <h2>Reviews</h2>
+            {reviews}
+          </div>
+      </ReviewContainerStyled>
+    )
+  }
+  
+  const ReviewContainerStyled = styled.div`
+   display: flex;`
+
+
+export default ReviewContainer;
