19eec08d5f086b5d502a0ac6b9aa2d8a82cde902
Jayvee Aspa <jayveeaspa@Jayvees-MacBook-Pro.local>
2019-08-01 10:04:07 -0700

    update signin/up with material ui

--- client/Components/AuthModalContent.jsx
+++ client/Components/AuthModalContent.jsx
@@ -3,13 +3,16 @@ import styled from 'styled-components';
 
 import Login from './Login';
 import SignUp from './SignUp';
+import Test from './Test';
 
 function AuthModalContent({ setAuthModal }) {
   const [willSignUp, setWillSignUp] = useState(false)
   return (
     <Styled>
       <button onClick={() => setAuthModal(false)}>X</button>
-      {willSignUp ? <SignUp setAuthModal={setAuthModal} /> : <Login setAuthModal={setAuthModal} setWillSignUp={setWillSignUp} />}
+      {willSignUp ? <SignUp setAuthModal={setAuthModal} /> : <Test setAuthModal={setAuthModal} setWillSignUp={setWillSignUp} />}
+      {/* <Login setAuthModal={setAuthModal} setWillSignUp={setWillSignUp} /> */}
+
     </Styled>
   )
 }
